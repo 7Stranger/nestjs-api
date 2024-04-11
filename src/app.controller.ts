be6554dd7f1db75fc133c77a9940808ab30a7364
7Stranger <stanislav.moiseeff@gmail.com>
2024-04-11 23:20:55 +0300

    Update vercel.json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { GetRequestIndexDto } from './dto/get-request-index.dto';
 
 @Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('liveness')
+  private liveness() {
+    return 'OK';
+  }
 
   @Post()
   private getRequestIndex(@Body() dto: GetRequestIndexDto): Promise<number> {
